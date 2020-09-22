581a8e5fcfd1ccd77096e4d7559fe0fc6d1b2dcd
mia-k-monaghan <66038823+mia-k-monaghan@users.noreply.github.com>
2020-09-22 15:33:16 -0700

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -8,10 +8,7 @@ urlpatterns = [
     path('<pk>/detail/', views.ItemDetailView.as_view(),name='detail'),
     path('create-payment-intent',views.createpayment,name='create-payment-intent'),
     path('payment-complete',views.paymentcomplete,name='payment-complete'),
-    path('<pk>/add-to-cart', views.add_to_cart, name='add'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('cart/', views.CartView.as_view(), name='cart'),
-    # path('subscribe/',views.SubscribeView.as_view(),name='subscribe'),
-    # path('')
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('shipping/', views.ShippingView.as_view(), name='shipping')
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,3 @@
-# import random
-# import string
-#
 from django.conf import settings
 from django.utils.decorators import method_decorator
 # from django.contrib import messages
@@ -9,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from djstripe.models import Product, Plan
-from .models import Cart
+from .models import Cart, Address as A
 from django.views.decorators.csrf import csrf_exempt
 # from django.utils import timezone
 from django.urls import reverse_lazy, reverse
@@ -20,111 +17,29 @@ from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404
 from .forms import SubscribeForm
 
-# stripe.api_key = settings.STRIPE_SECRET_KEY
-
-# Create your views here.
-# def create_ref_code():
-#     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
-#
-
 class HomeView(ListView):
     model = Product
     template_name = 'index.html'
     context_object_name = 'products'
-
-class CartView(ListView, LoginRequiredMixin):
-    model = Cart
-    template_name = 'cart.html'
-    context_object_name = 'cart'
-
-    def get_queryset(self):
-        filter_val = self.request.user
-        new_context = Cart.objects.filter(user=filter_val)
-        return new_context
-
-
 
 
 class ItemDetailView(DetailView):
     model = Product
     template_name = 'item_detail.html'
 
-def add_to_cart(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-
-    if product:
-        cart = Cart.objects.get_or_create(
-            user = request.user,
-            item = product
-        )
-        cart[0].save()
-
-    # add_cart = Profile.objects.get_or_create(
-    #     user = request.user,
-    #     cart = set(product)
-    # )
-    # add_cart.save()
-    return redirect('core:index')
 
 @login_required
 def checkout(request):
-    if request.user.is_authenticated:
-        form=SubscribeForm()
-        products = Product.objects.all()
-        product = Product.objects.filter(name='Classic Garden Party')[0]
-        total = Plan.objects.filter(product=product)[0].amount
-        return render(request, 'checkout.html', {'products':products, 'product':product, 'total':total, 'form':form})
-    else:
-        redirect('core:index')
+    form=SubscribeForm()
+    products = Product.objects.all()
+    product = Product.objects.filter(name='Classic Garden Party')[0]
+    total = Plan.objects.filter(product=product)[0].amount
 
-@csrf_exempt
-def createpayment(request):
-    if request.user.is_authenticated:
-        #TODO: get request product to work
-        product = Product.objects.filter(name='Classic Garden Party')[0]
-        total = Plan.objects.filter(product=product)[0].amount
-
-        stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
-        if request.method=="POST":
-
-            data = json.loads(request.body)
-            intent = stripe.PaymentIntent.create(
-                amount=int(total*100),
-                currency=data['currency'],
-                metadata={'integration_check': 'accept_a_payment'},
-
-                )
-            try:
-                return JsonResponse({'publishableKey':
-                    settings.STRIPE_TEST_PUBLIC_KEY, 'clientSecret': intent.client_secret})
-            except Exception as e:
-                return JsonResponse({'error':str(e)},status= 403)
-@csrf_exempt
-def paymentcomplete(request):
-    if request.method=='POST':
-        data = json.loads(request.POST.get('payload'))
-        if data['status'] == 'succeeded':
-            pass
-        return render(request, 'payment-complete.html')
-
-#
-class SubscribeView(View):
-
-    def get(self, *args,**kwargs):
-        form = SubscribeForm()
-        context= {
-            'form':form
-        }
-        return render(self.request, 'checkout.html', context)
-
-    def post(self, *args, **kwargs):
-        form = SubscribeForm(self.request.POST)
-        # pk = self.kwargs['pk']
-        # item = Product.objects.filter(pk=pk)[0]
-
+    if request.method=="POST":
+        form = SubscribeForm(request.POST)
         if form.is_valid():
             shipping_address = Address.objects.get_or_create(
-                user=self.request.user,
+                user=request.user,
                 street_address = form.cleaned_data.get('shipping_street_address'),
                 apartment_address = form.cleaned_data.get('shipping_apartment_address'),
                 state = form.cleaned_data.get('shipping_state'),
@@ -134,21 +49,102 @@ class SubscribeView(View):
             )
             shipping_address[0].save()
 
-            # billing_address = Address.objects.get_or_create(
-            #     user=self.request.user,
-            #     street_address = form.cleaned_data.get('billing_street_address'),
-            #     apartment_address = form.cleaned_data.get('billing_apartment_address'),
-            #     state = form.cleaned_data.get('billing_state'),
-            #     zip = form.cleaned_data.get('billing_zip'),
-            #     address_type = 'B'
-            #
-            # )
-            # billing_address[0].save()
+    return render(request, 'checkout.html', {'products':products, 'product':product, 'total':total, 'form':form})
 
-            # OrderSetting.objects.create(
-            #     user=self.request.user,
-            #     item=item,
-            #     shipping_address=shipping_address[0],
-            #     billing_address=billing_address[0]
-            # )
-        return HttpResponseRedirect(reverse('core:index'))
+@csrf_exempt
+@login_required
+def createpayment(request):
+    product = Product.objects.filter(name='Classic Garden Party')[0]
+    total = Plan.objects.filter(product=product)[0].amount
+
+    stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
+    if request.method=="POST":
+        data = json.loads(request.body)
+        intent = stripe.PaymentIntent.create(
+            amount=int(total*100),
+            currency=data['currency'],
+            metadata={'integration_check': 'accept_a_payment'},
+            )
+        try:
+            return JsonResponse({'publishableKey':
+                settings.STRIPE_TEST_PUBLIC_KEY, 'clientSecret': intent.client_secret})
+        except Exception as e:
+            return JsonResponse({'error':str(e)},status= 403)
+@csrf_exempt
+def paymentcomplete(request):
+    if request.method=='POST':
+        data = json.loads(request.POST.get('payload'))
+        if data['status'] == 'succeeded':
+            pass
+
+        return render(request, 'payment-complete.html')
+
+#
+class CheckoutView(LoginRequiredMixin,View):
+    def get(self, *args,**kwargs):
+        products = Product.objects.all()
+        product = Product.objects.filter(name='Classic Garden Party')[0]
+        total = Plan.objects.filter(product=product)[0].amount
+
+        context= {
+            'products':products,
+            'product':product,
+            'total':total
+        }
+        return render(self.request, 'checkout.html', context)
+
+
+class ShippingView(LoginRequiredMixin, View):
+
+    def get(self, *args,**kwargs):
+        form = SubscribeForm()
+
+        context= {
+            'form':form,
+        }
+        return render(self.request, 'shipping.html', context)
+
+    def post(self, *args, **kwargs):
+        form = SubscribeForm(self.request.POST)
+
+        if form.is_valid():
+            new_address = form.save(commit=False)
+            new_address.user = self.request.user
+            new_address.address_type = "S"
+            new_address.save()
+
+        return HttpResponseRedirect(reverse('core:checkout'))
+
+
+def createSub(request):
+    if request.method == 'POST':
+        street = request.POST.get('street')
+        apt = request.POST.get('apt')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip = request.POST.get('zip')
+
+        response_data = {}
+
+        address = A(
+            user = request.user,
+            street_address = street,
+            apartment_address = apt,
+            city = city,
+            state = state,
+            zip = zip,
+            address_type = 'S'
+        )
+        address.save()
+
+        response_data['result'] = 'Create post successful!'
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+            )
+    else:
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )

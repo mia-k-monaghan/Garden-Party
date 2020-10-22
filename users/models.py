75fe8a577602f0bdc1801f6f0365dae738fe1821
mia-k-monaghan <66038823+mia-k-monaghan@users.noreply.github.com>
2020-10-22 15:13:12 -0700

    Update models.py

--- users/models.py
+++ users/models.py
@@ -6,29 +6,29 @@ from djstripe.utils import subscriber_has_active_subscription
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-    customer = models.ForeignKey(
-        Customer, null=True, blank=True, on_delete=models.SET_NULL,
-        help_text="The user's Stripe Customer object, if it exists"
-    )
-    subscription = models.ForeignKey(
-        Subscription, null=True, blank=True, on_delete=models.SET_NULL,
-        help_text="The user's Stripe Subscription object, if it exists"
-    )
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(_('email address'), unique=True)
+#     customer = models.ForeignKey(
+#         Customer, null=True, blank=True, on_delete=models.SET_NULL,
+#         help_text="The user's Stripe Customer object, if it exists"
+#     )
+#     subscription = models.ForeignKey(
+#         Subscription, null=True, blank=True, on_delete=models.SET_NULL,
+#         help_text="The user's Stripe Subscription object, if it exists"
+#     )
 
-    @cached_property
-    def has_active_subscription(self):
-        """Checks if a user has an active subscription."""
-        return subscriber_has_active_subscription(self)
+#     @cached_property
+#     def has_active_subscription(self):
+#         """Checks if a user has an active subscription."""
+#         return subscriber_has_active_subscription(self)
 
-    def __str__(self):
-        return (self.email)
+#     def __str__(self):
+#         return (self.email)
 
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = self.email
-        super(CustomUser,self).save(*args,**kwargs)
+#     def save(self, *args, **kwargs):
+#         if not self.username:
+#             self.username = self.email
+#         super(CustomUser,self).save(*args,**kwargs)
 
 class LaunchSignUp(models.Model):
     email = models.EmailField(_('email address'), unique=True)
